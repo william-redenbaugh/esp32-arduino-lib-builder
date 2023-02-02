e88bc8ff83416e4ac8eb20143ae1699a658ab081
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-02-02 15:59:44 +0100

    Update install-esp-idf.sh

--- tools/install-esp-idf.sh
+++ tools/install-esp-idf.sh
@@ -28,6 +28,7 @@ fi
 #
 
 if [ ! -x $idf_was_installed ] || [ ! -x $commit_predefined ]; then
+        echo "$IDF_PATH submodule update"
 	git -C $IDF_PATH submodule update --init --recursive
 	$IDF_PATH/install.sh
 fi
