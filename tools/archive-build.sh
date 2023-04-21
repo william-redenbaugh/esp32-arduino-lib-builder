5a155ea7a9ceacdc66bec6016a0a7cd20bc81472
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-04-21 20:17:04 +0200

    rm get.exe py

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -21,6 +21,8 @@ rm -rf arduino-esp32/libraries/Insights
 rm -rf arduino-esp32/package
 rm -rf arduino-esp32/tools/esp32-arduino-libs
 rm -rf arduino-esp32/tools/esptool.py
+rm -rf arduino-esp32/tools/get.py
+rm -rf arduino-esp32/tools/get.exe
 rm -rf arduino-esp32/tools/ide-debug
 rm -rf arduino-esp32/tools/gen_insights_package.py
 rm -rf arduino-esp32/tools/gen_insights_package.exe
