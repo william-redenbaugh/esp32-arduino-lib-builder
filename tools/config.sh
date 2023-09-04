e7f8b9a1ccb8abe4237e2fd3c95b41754d1a99f9
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-09-04 18:06:22 +0200

    Update config.sh

--- tools/config.sh
+++ tools/config.sh
@@ -77,8 +77,8 @@ else
     AR_COMMIT=$(git -C "$AR_COMPS/arduino" rev-parse --short HEAD || echo "")
 fi
 
-rm -rf release-info.txt
-echo "Framework built from Tasmota IDF branch $IDF_BRANCH commit $IDF_COMMIT and $AR_REPO branch $AR_BRANCH commit $AR_COMMIT" >> release-info.txt
+#rm -rf release-info.txt
+#echo "Framework built from Tasmota IDF branch $IDF_BRANCH commit $IDF_COMMIT and $AR_REPO branch $AR_BRANCH commit $AR_COMMIT" >> release-info.txt
 
 function get_os(){
     OSBITS=`arch`
