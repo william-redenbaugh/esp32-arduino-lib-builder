5f011a7e5e9732b08e15219565dace54485b3c48
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-10-03 20:58:26 +0200

    ESP-LITTLEFS v1.10.0

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -24,17 +24,17 @@ if [ $? -ne 0 ]; then exit 1; fi
 cp "$AR_COMPS/esp32-camera/driver/private_include/cam_hal.h" "$AR_COMPS/esp32-camera/driver/include/"
 
 #
-# CLONE/UPDATE ESP-LITTLEFS branch https://github.com/joltwallet/esp_littlefs/tree/lfs2.8
+# CLONE/UPDATE ESP-LITTLEFS v1.10.0 commit 032f9...
 #
 echo "Updating ESP-LITTLEFS..."
 if [ ! -d "$AR_COMPS/esp_littlefs" ]; then
 	git clone $LITTLEFS_REPO_URL "$AR_COMPS/esp_littlefs"
- 	git -C "$AR_COMPS/esp_littlefs" checkout lfs2.8
+ 	git -C "$AR_COMPS/esp_littlefs" checkout 032f9eb2e291e7c4df63c3e6a3cb3bc766ded495
         git -C "$AR_COMPS/esp_littlefs" submodule update --init --recursive
 else
 	git -C "$AR_COMPS/esp_littlefs" fetch
 	git -C "$AR_COMPS/esp_littlefs" pull --ff-only
-        git -C "$AR_COMPS/esp_littlefs" checkout lfs2.8
+        git -C "$AR_COMPS/esp_littlefs" checkout 032f9eb2e291e7c4df63c3e6a3cb3bc766ded495
         git -C "$AR_COMPS/esp_littlefs" submodule update --init --recursive
 fi
 if [ $? -ne 0 ]; then exit 1; fi
