206a60f8b4d3291968153703d3fece227bff8a2d
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-10-09 10:56:18 +0200

    rm TFLiteMicro

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -22,6 +22,7 @@ rm -rf arduino-esp32/libraries/SPIFFS
 rm -rf arduino-esp32/libraries/BLE
 rm -rf arduino-esp32/libraries/SimpleBLE
 rm -rf arduino-esp32/libraries/WiFiProv
+rm -rf arduino-esp32/libraries/TFLiteMicro
 rm -rf arduino-esp32/libraries/ESP32
 rm -rf arduino-esp32/package
 rm -rf arduino-esp32/tools/esp32-arduino-libs
