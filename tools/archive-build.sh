85bae98d7606d20fc31824b6cc57360c3b7aee85
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-10-12 20:36:59 +0200

    rm -rf arduino-esp32/libraries/WiFiClientSecure

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -22,6 +22,7 @@ rm -rf arduino-esp32/libraries/SPIFFS
 rm -rf arduino-esp32/libraries/BLE
 rm -rf arduino-esp32/libraries/SimpleBLE
 rm -rf arduino-esp32/libraries/WiFiProv
+rm -rf arduino-esp32/libraries/WiFiClientSecure
 rm -rf arduino-esp32/libraries/TFLiteMicro
 rm -rf arduino-esp32/libraries/ESP32
 rm -rf arduino-esp32/package
