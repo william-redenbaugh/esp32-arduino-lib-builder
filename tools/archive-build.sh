d4194071ea3605a5124ab7d12a16b7b7a2522ff9
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-10-26 18:29:35 +0200

    rm examples / reduce build zip time

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -13,7 +13,16 @@ mkdir -p dist && rm -rf "$archive_path" "$build_archive_path"
 
 cd out
 echo "Creating PlatformIO Tasmota framework-arduinoespressif32"
-cp -rf ../components/arduino arduino-esp32
+mkdir -p arduino-esp32/cores/esp32
+mkdir -p arduino-esp32/tools/partitions
+cp -rf ../components/arduino/tools arduino-esp32
+cp -rf ../components/arduino/cores arduino-esp32
+cp -rf ../components/arduino/libraries arduino-esp32
+cp -rf ../components/arduino/variants arduino-esp32
+cp -f ../components/arduino/CMa* arduino-esp32
+cp -f ../components/arduino/idf* arduino-esp32
+cp -f ../components/arduino/Kco* arduino-esp32
+cp -f ../components/arduino/pac* arduino-esp32
 rm -rf arduino-esp32/docs
 rm -rf arduino-esp32/tests
 rm -rf arduino-esp32/libraries/RainMaker
@@ -28,7 +37,6 @@ rm -rf arduino-esp32/libraries/WiFiClientSecure
 rm -rf arduino-esp32/libraries/ESP_SR
 rm -rf arduino-esp32/libraries/TFLiteMicro
 rm -rf arduino-esp32/libraries/ESP32
-rm -rf arduino-esp32/**/examples
 rm -rf arduino-esp32/package
 rm -rf arduino-esp32/tools/esp32-arduino-libs
 rm -rf arduino-esp32/tools/esptool.py
@@ -46,6 +54,9 @@ cp -Rf tools/esp32-arduino-libs arduino-esp32/tools/
 cp ../package.json arduino-esp32/package.json
 cp ../core_version.h arduino-esp32/cores/esp32/core_version.h
 mv arduino-esp32/ framework-arduinoespressif32/
+cd framework-arduinoespressif32/libraries
+rm -rf **/examples
+cd ../../
 # If the framework is needed as tar.gz uncomment next line
 # tar --exclude=.* -zcf ../$pio_archive_path framework-arduinoespressif32/
 7z a -mx=9 -tzip -xr'!.*' ../$pio_zip_archive_path framework-arduinoespressif32/
